4b19f7ed5d17ab9134be9338994091aab1d8fd4a
kris1k <165261082+kris1k@users.noreply.github.com>
2025-03-13 15:35:00 +0500

    Add files via upload

---
+++ 2kalkul/database.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# Создаем и подключаемся к базе данных
+conn = sqlite3.connect('tariffs.db')
+c = conn.cursor()
+
+# Создаем таблицу
+c.execute('''
+CREATE TABLE IF NOT EXISTS tariffs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    monthly_price REAL,
+    after_break_price REAL,
+    has_unlimited_line BOOLEAN,
+    remote_consultation_hours INTEGER,
+    service_engineer_hours INTEGER,
+    info_bases_count TEXT,
+    cabinet_employee_service TEXT,
+    spark_risks_service TEXT
+)
+''')
+
+# Вставляем данные
+c.execute('''
+INSERT INTO tariffs (name, monthly_price, after_break_price, has_unlimited_line,
+remote_consultation_hours, service_engineer_hours, info_bases_count,
+cabinet_employee_service, spark_risks_service) VALUES 
+('Минимальный', 4297, 5156, 1, 1, 1, 'Не ограничено', 'Приобретается дополнительно', 'Приобретается дополнительно'),
+('Оптимальный', 6535, 7394, 1, 2, 1, 'Не ограничено', 'На 25 сотрудников', 'Приобретается дополнительно'),
+('Сервисный', 8592, 9451, 1, 3, 2, 'Не ограничено', 'На 10 сотрудников', 'Приобретается дополнительно');
+''')
+
+# Сохраняем изменения
+conn.commit()
+conn.close()
